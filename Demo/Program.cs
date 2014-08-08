e1212de86667d1f597f52de97e410c31f3104692
zanpen2000 <zanpen2000@gmail.com>
2014-08-08 17:29:35 +0800

    demo program added

---
+++ Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now.Ticks;
+            var files = System.IO.Directory.GetFiles("f:\\", "*", System.IO.SearchOption.AllDirectories);
+            var done = DateTime.Now.Ticks;
+
+            var over = done - now;
+            Console.WriteLine(over.ToString());
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
